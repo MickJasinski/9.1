dd3e0206b4b41b7a1cac6b653085289eaadee946
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-11-30 12:47:56 +0000

    Add console.log's

--- 09.1/js/scripts.js
+++ 09.1/js/scripts.js
@@ -1,19 +1,23 @@
 // scripts.js
 
 function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
-		return "Data error!";
-	} else {
-		return a * h / 2;
-	}
-	console.log(getTriangleArea(10, 6));
+    if (a <= 0 || h <= 0) {
+        return "Data error!";
+    } else {
+        return a * h / 2;
+    }
 
 }
 
-var triangle1Area = getTriangleArea(10, 15) 
+
+var triangle1Area = getTriangleArea(10, 15);
+console.log('Value of variable: ' + triangle1Area);
 
 var triangle2Area = getTriangleArea(12, 25);
+console.log('Value of variable: ' + triangle2Area);
 
 var triangle3Area = getTriangleArea(8, 7);
+console.log('Value of variable: ' + triangle3Area);
 
 var triangle4Area = getTriangleArea(-4, 0);
+console.log('Value of variable: ' + triangle4Area);
